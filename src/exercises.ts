be431ca65269d56d5d924cbb4ad76e6eb67fa74e
Hugo Sousa <hugo.sousa@scaling.ch>
2023-12-27 18:32:25 +0100

    Add 'Verben im Präteritum' and 'Zeitangaben Präpositionen'

--- src/exercises.ts
+++ src/exercises.ts
@@ -24,6 +24,19 @@ const verbenImPerfekt = [
     exercise("Conrad Zuse hat 1937 den ersten frei programmierbaren Computer _____. (erfinden)", "erfunden"),
 ]
 
+const verbenImPraeteritum = [
+    exercise("Wegen des schlechten Wetters _____ der Wettkampf abgesagt. (werden)", "wurde"),
+    exercise("Trotz des Strassenlärms _____ ich mich gut konzentrieren. (können)", "konnte"),
+    exercise("Trotz seines Erfolges _____ er stolz auf sich. (sein)", "war"),
+    exercise("Trotz seiner Erkältung _____ der Sänger das Konzert abbrechen. (müssen)", "musste"),
+    exercise("Trotz der hohen Personalkosten _____ die Firma in diesem Jahr einen Gewinn. (erzielen)", "erzielte"),
+    exercise("Wegen der hohen Personalkosten _____ die Firma in diesem Jahr Verluste. (machen)", "machte"),
+    exercise("Wegen des fleissigen Trainings _____ sie die Silbermedaille. (erringen)", "errang"),
+    exercise("Trotz der Massnahmen der Regierung _____ sich die Lage auf dem Arbeitsmarkt nicht. (verbessern)", "verbesserte"),
+    exercise("Wegen der Massnahmen der Regierung _____ die Zahl der Arbeitslosen. (sinken)", "sank"),
+    exercise("Trotz der vielen Gespräche _____ man keine Lösund. (finden)", "fand"),
+]
+
 const verbenMitPraepositionen = [
     exercise("Verzichten Sie doch ___ Klatsch und Trasch!", "auf"),
     exercise("Erinnerst du dich oft ___ deine Studienzeit?", "an"),
@@ -36,8 +49,21 @@ const verbenMitPraepositionen = [
     exercise("Die meisten Künstler in Deutschland leben ___ sehr wenig Geld.", "von"),
 ]
 
+const zeitangabenPraepositionen = [
+    exercise("___ Nachmittag besuden wir eine Ausstellung", "Am"),
+    exercise("Wir sehen uns ___ drei Wochen.", "in"),
+    exercise("Kommst du ___ Samstag mit zum Fussball?", "am"),
+    exercise("___ Moment habe ich leider keine Zeit.", "Im"),
+    exercise("Er hat sich ___ Skifahren das Bein gebrochen.", "beim"),
+    exercise("Der Kunde kommt ___ 13. Mai.", "am"),
+    exercise("Der Kunde kommt ___ 13.30 Uhr.", "um"),
+    exercise("Was habit ihr ___ des Urlaubs gemacht?", "während"),
+    exercise("___ Juli fahren wir nach Spanien.", "Im"),
+]
 
 export const exercises: Record<string, Exercise> = {
     "Verben im Perfekt": verbenImPerfekt,
+    "Verben im Präteritum": verbenImPraeteritum,
     "Verben mit Präpositionen": verbenMitPraepositionen,
+    "Zeitangaben Präpositionen": zeitangabenPraepositionen,
 };
